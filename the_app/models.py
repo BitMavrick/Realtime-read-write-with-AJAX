38f9ea71019ff1563a8cd2aad855750f538ab558
Mehedi Hasan <mehedi705bd@gmail.com>
2022-01-29 13:58:03 +0600

    Create and update the database

--- the_app/models.py
+++ the_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class data(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
